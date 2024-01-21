f72c3c15c4f0a6897c49c6394ec791aa3cf0b83a
Xrddr <danilapetrukhin@mail.ru>
2024-01-21 23:04:54 +0400

    Add about layout

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', 'About')
+@section('content')
+    <div class="p-5 text-center border rounded">
+        <h1 class="text-body-emphasis">About the site</h1>
+        <p class="col-lg-10 mx-auto fs-3 text-muted">
+            Here will be information about the site at some point.
+        </p>
+        <code>"Love is the one thing we're capable of perceiving that transcends dimensions of time and space" -
+            "Interstellar, Brand"</code>
+    </div>
+@endsection
