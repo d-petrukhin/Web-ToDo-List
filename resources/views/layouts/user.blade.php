95b7b77ec85d35798b21747482f2df79c0a3905f
Xrddr <danilapetrukhin@mail.ru>
2024-01-27 00:09:26 +0400

    Update layout for user.blade.php

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,22 +1,24 @@
 @if(Auth::check())
     <div class="dropdown">
-        <button type="button" class="btn btn-outline-light dropdown-toggle" data-bs-toggle="dropdown"
+        <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown"
                 aria-expanded="false" data-bs-offset="0,15">
             {{ Auth::user()->name }}
         </button>
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="{{ route('profile.show', Auth::user()) }}">Profile</a></li>
+            <li><a class="dropdown-item" href="{{ route('settings', Auth::user()) }}">Settings</a></li>
             <li>
                 <hr class="dropdown-divider">
             </li>
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <li><button class="dropdown-item" type="submit">Log out</button></li>
+                <li>
+                    <button class="dropdown-item" type="submit">Log out</button>
+                </li>
             </form>
         </ul>
     </div>
 @else
-    <a href="{{ route('login') }}" type="button" class="btn btn-outline-light me-2">Sign in</a>
-    <a href="{{ route('register') }}" type="button" class="btn btn-outline-light">Sign up</a>
+    <a href="{{ route('login') }}" type="button" class="btn btn-dark me-2">Sign in</a>
+    <a href="{{ route('register') }}" type="button" class="btn btn-dark">Sign up</a>
 @endif
