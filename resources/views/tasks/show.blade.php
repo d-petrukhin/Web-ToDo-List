1139ce0312346d2c5ac373c3ed1856ebf14f3741
Xrddr <danilapetrukhin@mail.ru>
2024-01-27 00:11:49 +0400

    Update show tasks layout

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,42 +1,49 @@
 @extends('layouts.app')
 @section('title', 'Task')
 @section('content')
-    <div class="p-5 text-center border rounded">
-        <h1 class="text-body-emphasis">Task information</h1>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">status completed</th>
-                <th scope="col">title</th>
-                <th scope="col">description</th>
-                <th scope="col">created at</th>
-                <th scope="col">updated at</th>
-                <th scope="col">user</th>
-            </tr>
-            </thead>
-            <tbody class="table-group-divider">
-            <tr>
-                <th scope="row">{{ $task->id }}</th>
-                <th scope="row">{{ $task->completed ? 'true' : 'false' }}</th>
-                <td>{{ $task->title }}</td>
-                <td>{{ $task->description }}</td>
-                <td>{{ $task->created_at }}</td>
-                <td>{{ $task->updated_at }}</td>
-                <td>{{ Auth::user()->name }}</td>
-            </tr>
-        </table>
-        <div class="row row-cols-auto">
-            <div class="col">
-                <a href="{{ route('tasks.edit', $task) }}" type="button" class="btn btn-outline-primary">Edit task</a>
-            </div>
-            <div class="col">
-                <form method="POST" action="{{ route('tasks.destroy', $task) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-outline-danger">Destroy task</button>
-                </form>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title text-center text-body-emphasis">Task Information</h1>
+
+                <table class="table table-bordered mt-4">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Status Completed</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Updated At</th>
+                        <th scope="col">Owner</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>{{ $task->id }}</td>
+                        <td>{{ $task->completed ? 'True' : 'False' }}</td>
+                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->description }}</td>
+                        <td>{{ $task->created_at }}</td>
+                        <td>{{ $task->updated_at }}</td>
+                        <td>{{ $task->user->name }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <div class="row row-cols-auto">
+                    <div class="col">
+                        <a href="{{ route('tasks.edit', $task) }}" type="button" class="btn btn-outline-primary">Edit
+                            task</a>
+                    </div>
+                    <div class="col">
+                        <form method="POST" action="{{ route('tasks.destroy', $task) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-danger">Destroy task</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 @endsection
