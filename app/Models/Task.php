e6a2688ac9d0ff17e9e2601825f05025baed6f87
Xrddr <danilapetrukhin@mail.ru>
2024-01-26 23:56:24 +0400

    Update model for Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -18,5 +19,10 @@ class Task extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function isOwnedByUser()
+    {
+        return $this->user_id === Auth::id();
     }
 }
