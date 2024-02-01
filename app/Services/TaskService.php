7670079403d2f30da54d3e9d3ae4a24f9d6caa8f
Xrddr <danilapetrukhin@mail.ru>
2024-02-01 22:35:16 +0400

    Add method updated completed task on click checkbox

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -4,6 +4,13 @@ namespace App\Services;
 
 class TaskService
 {
+    public function updateTask($task, array $data)
+    {
+        $data['completed'] = !empty($data['completed']);
+
+        $task->update($data);
+    }
+
     public function getTotalTasks($user)
     {
         return $user->tasks()->count();
