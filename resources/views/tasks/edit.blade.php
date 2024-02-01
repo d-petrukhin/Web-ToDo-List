ef73f61df6dc80fdb98acae3401ac1b829c0776f
Xrddr <danilapetrukhin@mail.ru>
2024-02-01 22:31:27 +0400

    Include components for edit form

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('title', 'Edit task')
 @section('content')
-    @include('tasks.form', ['action' => route('tasks.update', $task), 'method' => 'PUT', 'btn' => 'Edit task', 'title' => 'Your old information: '.$task->title, 'description' => 'Your old information: '.$task->description])
+    @include('tasks.form', [
+    'action' => route('tasks.update', $task),
+    'method' => 'PUT',
+    'btn' => 'Edit task',
+    'title' => $task->title,
+    'description' => $task->description,
+    'additionalContent' => view('tasks.components.additionalFieldsForEditForm', compact('task'))])
 @endsection
