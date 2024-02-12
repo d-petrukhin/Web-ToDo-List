5dfc4f32efb6301e6aa335d331a4018aa1220248
Xrddr <danilapetrukhin@mail.ru>
2024-02-12 19:28:53 +0300

    delete settings profile and add sitemap route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => ''], static function () {
     Route::get('/', [HomeController::class, 'home'])->name('home');
     Route::get('about', [AboutController::class, 'about'])->name('about');
-    Route::get('profile/{id}/settings', [SettingsController::class, 'settings'])->name('settings');
 });
 
 Route::middleware('auth')->resource('tasks', TaskController::class);
 
 Route::middleware('auth')->resource('profile', ProfileController::class);
+
+Route::get('/sitemap', function () {
+    return response()->file(public_path('sitemap.xml'));
+})->name('sitemap');
+
 
 require __DIR__.'/auth.php';
