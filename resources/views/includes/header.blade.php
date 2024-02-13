2017031fc93d1de06c912e07fa0f4aeae1a300d5
Xrddr <danilapetrukhin@mail.ru>
2024-02-13 21:59:40 +0300

    Use Blade-component for dropdown menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,12 +10,13 @@
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li><a href="{{ route('home') }}" class="nav-link px-2 link-secondary text-white">Home</a></li>
-            <li><a href="{{ route('tasks.create') }}" class="nav-link px-2 link-secondary text-white">Create task</a></li>
+            <li><a href="{{ route('tasks.create') }}" class="nav-link px-2 link-secondary text-white">Create task</a>
+            </li>
             <li><a href="{{ route('tasks.index') }}" class="nav-link px-2 link-secondary text-white">Show tasks</a></li>
             <li><a href="{{ route('about') }}" class="nav-link px-2 link-secondary text-white">About</a></li>
         </ul>
         <div class="col-md-3 text-end">
-            @include('layouts.user')
+            <x-user-dropdown />
         </div>
     </header>
 </div>
