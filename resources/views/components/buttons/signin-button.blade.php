4d9902096c550c829fde128768bb38a78d424ae3
d-petrukhin <62542391+d-petrukhin@users.noreply.github.com>
2024-02-15 23:52:07 +0400

    Revert "Feature/app refactiring components"

--- resources/views/components/buttons/signin-button.blade.php
+++
@@ -1,1 +0,0 @@
-<a href="{{ route('login') }}" type="button" class="btn btn-dark me-2">Sign in</a>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,18 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => ''], static function () {
-    Route::get('/', [HomeController::class, 'home'])->name('home');
-    Route::get('about', [AboutController::class, 'about'])->name('about');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::middleware('auth')->resource('tasks', TaskController::class);
-
-Route::middleware('auth')->resource('profile', ProfileController::class);
-
-Route::get('/sitemap', function () {
-    return response()->file(public_path('sitemap.xml'));
-})->name('sitemap');
-
-
-require __DIR__.'/auth.php';
