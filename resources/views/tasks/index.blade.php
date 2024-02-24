11e91d87de2efd666df4d7a175c1c667edd413d0
d-petrukhin <danilapetrukhin@mail.ru>
2024-02-24 21:37:52 +0300

    Update. Fix pagination.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,13 +7,14 @@
             <h6 class="pb-2 mb-0">Your tasks:</h6>
             @foreach($tasks as $task)
                 <div class="list-group row">
-                    <ul class="list-group-item d-flex gap-2" >
+                    <ul class="list-group-item d-flex gap-2">
                         <div class="col">
                             <input class="form-check-input me-1" type="checkbox" value="1" name="completed"
                                    id="firstCheckbox" disabled {{ $task->completed ? 'checked' : '' }} ">
                         </div>
                         <div class="col-10">
-                            <a href="{{ route('tasks.show', $task) }}" class="link-offset-1 link-underline link-underline-opacity-0 list-group w-75">
+                            <a href="{{ route('tasks.show', $task) }}"
+                               class="link-offset-1 link-underline link-underline-opacity-0 list-group w-75">
                                 {{ $task->title }}
                                 <small class="d-block text-body-secondary">{{ $task->description }}</small></a>
                         </div>
@@ -24,12 +25,14 @@
                             <form method="POST" action="{{ route('tasks.destroy', $task) }}" class="d-flex">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                <button type="submit" class="btn btn-outline-danger"
+                                        onclick="return confirm('Are you sure?')">Delete
+                                </button>
                             </form>
                         </div>
                     </ul>
                 </div>
             @endforeach
-            @include('layouts.paginate')
+            @include('layouts.paginate', ['param' => $tasks])
         </div>
 @endsection
