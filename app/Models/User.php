01c8dcc3827b8bc5ad5033ffa8b75f372142f511
d-petrukhin <danilapetrukhin@mail.ru>
2024-02-26 22:00:32 +0300

    Update. Refactoring User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -48,13 +49,13 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function tasks()
+    public function tasks(): HasMany
     {
-        return $this->hasMany(Task::class, 'user_id', 'id');
+        return $this->hasMany(Task::class);
     }
 
-    public function folders()
+    public function folders(): HasMany
     {
-        return $this->hasMany(Folder::class, 'user_id', 'id');
+        return $this->hasMany(Folder::class);
     }
 }
