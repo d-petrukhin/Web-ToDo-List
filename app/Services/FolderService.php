34999bc22c6530c95b6640daf4c88b0634bca829
d-petrukhin <danilapetrukhin@mail.ru>
2024-02-28 21:32:54 +0300

    Update. Refactioring method delete folder and tasks.

--- app/Services/FolderService.php
+++ app/Services/FolderService.php
@@ -6,9 +6,9 @@ use App\Models\User;
 
 class FolderService
 {
-    public function deleteFolderAndTasks(User $user, $id): bool
+    public function deleteFolderAndTasks(User $user, $folder): bool
     {
-        $folder = $user->folders()->findOrFail($id);
+        $folder = $user->folders()->findOrFail($folder->id);
 
         $user->tasks()->where('folder_id', $folder->id)->delete();
 
